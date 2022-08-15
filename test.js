9a035a07b2d07eb8eb6e71e8a205b5e33f3719f9
jeff mascarenhas <>
2022-08-15 17:51:19 -0300

    feat: hero updated

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { deepEqual, ok } from "assert";
+import { deepEqual, equal, throws } from "assert";
 
 import database from "./database.js";
 
@@ -9,9 +9,57 @@ const DEFAULT_ITEM_REGISTERED = {
 };
 
 describe("Suit of manipulation of heroes", () => {
+  before(async () => {
+    await database.register(DEFAULT_ITEM_REGISTERED);
+  });
+
   it("should search a specific hero on the list of heroes", async () => {
     const expected = DEFAULT_ITEM_REGISTERED;
     const [result] = await database.list(expected.id); // destructuring was used to get the first position of array
     deepEqual(result, expected);
+  });
+
+  it("should register a hero, using files", async () => {
+    const newHero = {
+      id: 2,
+      name: "Batman",
+      power: "Smart",
+    };
+    const expected = newHero;
+    await database.register(newHero);
+    const [actual] = await database.list(newHero.id);
+
+    deepEqual(actual, expected);
+  });
+
+  it("should remove a hero", async () => {
+    const expected = true;
+    const result = await database.remove(DEFAULT_ITEM_REGISTERED.id);
+
+    equal(result, expected);
+  });
+
+  // it("should return error 'User does not exists'", async () => {
+  //   const expected = new Error("User does not exists");
+  //   const result = await database.remove(100);
+  //   console.log(result);
+  //   throws(() => {
+  //     throw expected;
+  //   }, result);
+  // });
+
+  it("should update hero by id", async () => {
+    const hero = {
+      id: DEFAULT_ITEM_REGISTERED.id,
+      name: "Black Flash",
+      power: "Super Speed",
+    };
+
+    const expected = hero;
+    await database.update(DEFAULT_ITEM_REGISTERED.id, hero);
+
+    const [actual] = await database.list(DEFAULT_ITEM_REGISTERED.id);
+
+    deepEqual(actual, expected);
   });
 });
